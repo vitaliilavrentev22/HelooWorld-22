02c5dbb7a51de4893a18380479b4ba67e5ef5f8f
vitaliilavrentev22 <122769637+vitaliilavrentev22@users.noreply.github.com>
2023-01-19 08:26:11 +0300

    2

---
+++ Module2.vb
@@ -0,0 +1,31 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim result As Long = 0
+        Dim user_input As Long
+
+
+        Console.WriteLine("Введите начальное число: ")
+        result = Console.ReadLine()
+
+        While True
+            Console.WriteLine("Введите число для перемножения: ")
+            user_input = Int64.Parse(Console.ReadLine())
+
+            If user_input = 1 Then
+                Console.WriteLine("Результат перемножения: {0}", result)
+                Exit While
+            End If
+
+            If result > Int64.MaxValue / user_input Then
+                Console.WriteLine("Произошло переполнение числа! Результат умножения до переполнения: {0}", result)
+                Exit While
+            End If
+
+            result *= user_input
+        End While
+        Console.ReadKey()
+
+    End Sub
+
+End Module
