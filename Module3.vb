7059eed1d5b254a2bf0bc330d0f3e3609e4714c1
vitaliilavrentev22 <122769637+vitaliilavrentev22@users.noreply.github.com>
2023-01-26 08:25:42 +0300

    дз3

---
+++ Module3.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+    Sub Main(args As String())
+        Console.WriteLine("Введите строку текста: ")
+
+        Dim text_line As String = Console.ReadLine()
+
+        Dim vowels As Integer = 0, consonants = 0, spaces = 0, punctuation_marks = 0
+
+        For Each symbol As Char In text_line
+            Select Case symbol
+                Case "A"c To "Z"c
+                    vowels += 1
+                Case "a"c To "z"c
+                    consonants += 1
+                Case " "c
+                    spaces += 1
+                Case ","c To "/"c, ";"c, ":"c, "?"c
+                    punctuation_marks += 1
+            End Select
+
+    End Sub
+
+End Module
