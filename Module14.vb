440904fe8319f589ad60eb0943e5e2ac1300e053
vitaliilavrentev22 <122769637+vitaliilavrentev22@users.noreply.github.com>
2023-04-03 08:57:31 +0300

    Add files via upload

--- Module14.vb
+++ Module14.vb
@@ -1,56 +1,15 @@
 ﻿Imports System
 
-Module ìàññèâû2
-    Sub Main(args As String())
+Sub Main(args As String())
 
-        Dim input(4) As Integer
-        Console.WriteLine(" Set 5 numbers ")
+    Dim input(4) As Integer
+    Console.WriteLine(" Set 5 numbers ")
 
-        For i = 0 To 4
-            input(i) = Console.ReadLine()
-        Next
-        Dim num As Integer
+    For i = 0 To 4
+        input(i) = Console.ReadLine()
+    Next
+    Dim num As Integer
 
-        Console.WriteLine(find(input, num))
+    Console.WriteLine(find(input, num))
 
-    End Sub
-
-    Sub printrevers(list As Integer())
-        For i = list.Length - 1 To 0 Step -1
-            Console.WriteLine(list(i))
-        Next
-    End Sub
-
-    Sub printrevers(list As String())
-        For i = list.Length - 1 To 0 Step -1
-            Console.WriteLine(list(i))
-        Next
-    End Sub
-
-    Sub print(list As Integer())
-
-        For i = 0 To list.Length - 1
-            Console.WriteLine(list(i))
-        Next
-    End Sub
-
-    Sub print(list As String())
-
-        For i = 0 To list.Length - 1
-            Console.WriteLine(list(i))
-        Next
-    End Sub
-
-    Function find(list As Integer(), num As Integer) As Integer
-
-        For i = 0 To list.Length - 1
-            If list(i) = 22 Then
-                find = i
-                Exit Function
-            End If
-        Next
-
-        find = -1
-
-    End Function
-End Module
+End Sub
