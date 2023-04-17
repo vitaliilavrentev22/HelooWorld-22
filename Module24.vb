e8baed0e2f44762f95dee1996c43f279844619f2
vitaliilavrentev22 <122769637+vitaliilavrentev22@users.noreply.github.com>
2023-04-17 08:46:01 +0300

    Independent work

---
+++ Module24.vb
@@ -0,0 +1,13 @@
+﻿Module Program
+    Sub Main(args As String())
+        Dim t, z, x, c, ztc, y As Double
+        z = 5.2
+        t = 6.7
+        c = -2.5
+        ztc = 5.2 * 6.7 * -2.5
+        t = MathF.Sqrt(MathF.Abs(4 * MathF.Pow(x, 2) - x * z + c))
+        Console.WriteLine(t)
+        x = MathF.Log(MathF.Pow(z * c - t) - 2) / Math.F(MathF.E, z) - MathF.Sin(MathF.Sin, 2) * MathF.Pow(MathF.z, 2)
+        Console.WriteLine(x)
+    End Sub
+End Module
