e3f4ef8acb46f7d4a3ad0210a96f5918a414116f
vitaliilavrentev22 <122769637+vitaliilavrentev22@users.noreply.github.com>
2023-04-17 08:58:09 +0300

    Самостоятельная правильная

---
+++ Module25.vb
@@ -0,0 +1,13 @@
+﻿Module Program
+    Sub Main(args As String())
+        Dim t, z, x, c, ztc, y As Double
+        z = 5.2
+        t = 6.7
+        c = -2.5
+        ztc = 5.2 * 6.7 * -2.5
+        t = MathF.Sqrt(MathF.Abs(4 * MathF.Pow(x, 2) - x * z + c))
+        Console.WriteLine(t)
+        x = MathF.Log(MathF.Abs(z * c - t) - 2) / MathF.Pow(MathF.E, z) - MathF.Pow(Sin, 2) * MathF.Pow(z, 2)
+        Console.WriteLine(x)
+    End Sub
+End Module
