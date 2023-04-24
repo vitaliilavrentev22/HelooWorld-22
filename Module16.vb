cc565c113f401f7f3390dc22aac01cdabc8c303b
vitaliilavrentev22 <122769637+vitaliilavrentev22@users.noreply.github.com>
2023-04-24 21:17:59 +0300

    Tasks to get 4
    I watched the videos that we made and parsed with arrays and did them

--- Module16.vb
+++ Module16.vb
@@ -1,32 +1,258 @@
 ﻿Imports System
+Imports System.Net
 
-Module ÏîèñêÝëåìåíòàâÌàññèâåфвф
+Module Masiv
+    Enum sortDirectionъ
+        fewrwegf
+        Vfdret
+    End Enum
     Sub Main(args As String())
-
-        Dim input(4) As Integer
-        Console.WriteLine(" Set 5 numbers ")
+        Dim num(4), Mas As Integer
+        'Dim Slovo() As String
+        Console.WriteLine("have 5 Name")
+        Dim m, n As Integer
 
         For i = 0 To 4
-            input(i) = Console.ReadLine()
+            num(i) = Console.ReadLine()
         Next
 
-        Dim num As Integer
+        'printRevers(Slovo)
 
-        Console.WriteLine(" What number to look for? ")
+        'Console.WriteLine("index = {0}", find(num, 21))
 
-        num = Console.ReadLine
+        'Dim strings(4) As String
 
-        Console.WriteLine(find(input, num))
+        'For i = 0 To strings.Length - 1
+        ' strings(i) = Console.ReadLine()
+        'Next
+        ''printRevers(strings)
+
+        'Console.WriteLine("Write 5 values (Килограммы)")
+
+        'For i = 0 To 4
+        ' num(i) = Console.ReadLine()
+        'Next
+        'Console.WriteLine("Numbers")
+        'For i = 0 To 4
+        ' num(i) = Console.ReadLine()
+        'Next
+        ''n = min(strings)
+        ''m = max(strings)
+        ''Console.WriteLine("index = {0}, Min = {1}", n, strings(n))
+        ''Console.WriteLine("index = {0}, Max = {1}", m, strings(m))
+        ''add(num, "add", 2)
+
+
+        'printRevers(strings)
+        'printRevers(num)
+        'Console.WriteLine("index = {0}", minInt(num))
+        'delete(num, 2)
+        'print(num)
+        'Dim minIndex As Integer = min(num)
+        'If minIndex > 0 Then
+        ' Dim Boofer As String = num(0)
+        ' num(0) = num(minIndex)
+        ' num(minIndex) = Boofer
+        'End If
+        Sort(num, sortDirection.Vfdret)
 
     End Sub
+    Sub printRevers(list As Integer())
 
-    Sub printrevers(list As Integer())
         For i = list.Length - 1 To 0 Step -1
             Console.WriteLine(list(i))
         Next
+
+    End Sub
+    Sub printRevers(list As String())
+
+        For i = 0 To list.Length - 1
+            Console.WriteLine(list(i))
+        Next
+
+    End Sub
+    Sub print(list As Integer())
+
+        For i = 0 To list.Length - 1
+            Console.WriteLine(list(i))
+        Next
+
+    End Sub
+    Function find(list As Integer(), e As Integer) As Integer
+
+        For f = 0 To list.Length - 1
+            If list(f) = e Then
+                find = f
+                Exit Function
+            End If
+        Next
+        find = -1
+
+    End Function
+
+    Function findStr(list As String(), e As String) As Integer
+
+        For f = 0 To list.Length - 1
+            If list(f) = e Then
+                findStr = f
+                Exit Function
+            End If
+        Next
+        findStr = -1
+
+    End Function
+    Function max(list As Integer(), Optional start As Integer = 0) As Integer
+        max = 0
+        For i = 1 To list.Length - 1
+            If list(max) < list(i) Then
+                max = i
+            End If
+        Next
+
+    End Function
+
+    Function min(list As Integer(), Optional start As Integer = 0) As Integer
+        min = start
+        For i = start To list.Length - 1
+            If list(min) > list(i) Then
+                min = i
+            End If
+        Next
+
+    End Function
+
+    'Function minInt(list As Integer()) As Integer
+    ' minInt = 0
+    ' For i = 1 To list.Length - 1
+    ' If list(minInt) > list(i) Then
+    ' minInt = i
+    ' End If
+    ' Next
+
+    'End Function
+
+    Sub add(ByRef list As String(), val As String, index As Integer)
+        If index < 0 Or index > list.Length Then Exit Sub
+        ReDim Preserve list(list.Length)
+        For i = list.Length - 1 To index + 1 Step -1
+            list(i) = list(i - 1)
+        Next
+        list(index) = val
     End Sub
 
-    Sub printrevers(list As String())
+    Sub delete(ByRef list As String(), index As Integer)
+        If index < 0 Or index >= list.Length Then Exit Sub
+        For i As Integer = index To list.Length - 2
+
+        Next
+        ReDim Preserve list(list.Length - 2)
+    End Sub
+
+    Sub Sort(list As Integer(), derection As Boolean)
+        Dim index As Integer
+        Dim boofer As Integer
+        For i = 0 To list.Length - 1
+            If derection Then
+                index = min(list, i)
+            ElseIf sortDirection.Vfdret Then
+                index = max(list, i)
+            End If
+            If index > i Then
+                boofer = list(index)
+                list(index) = list(i)
+                list(i) = boofer
+            End If
+        Next
+    End Sub
+    Sub Sort1(list As Integer(), Optional start As Integer = 0)
+        Dim Maxindex As Integer
+        Dim boofer As Integer
+        For i = 0 To list.Length - 1
+            If Maxindex > i Then
+                boofer = list(Maxindex)
+                list(Maxindex) = list(i)
+                list(i) = boofer
+            End If
+            Maxindex = max(list, i)
+        Next
+    End Sub
+End Module
+Module Program
+    Enum SortDirection
+        Vozrastanie
+        Ybivanie
+    End Enum
+    Sub Main(args As String())
+
+        Console.WriteLine("Enter five numbers:")
+        Dim numbers(4) As Integer
+        Dim n As Integer
+        For i = 0 To 4
+            numbers(i) = Console.ReadLine
+        Next
+        Console.WriteLine("Choose what you need: 1 - Find a number in the array, 2 - Find the minimum number in the array, 3 - Find the maximum number in the array, 4 - Output the array in reverse order, 5 - Output the array in the usual order, 6 - Add a new element to the array, 7 - Remove an element from the array, 8 - Sort the numbers in ascending order, 9 - Sort the numbers in descending order:")
+        n = Console.ReadLine
+        Select Case n
+            Case 1
+
+                Console.WriteLine("Which number do you want to search for?")
+                n = Console.ReadLine
+                Console.ForegroundColor = ConsoleColor.Green
+                Console.WriteLine("----------------------------------------------------------")
+                Console.WriteLine("The number you are looking for is in the {0} index", find(numbers, n))
+                Console.WriteLine("----------------------------------------------------------")
+
+            Case 2
+                Console.ForegroundColor = ConsoleColor.Green
+                Console.WriteLine("----------------------------------------------------------")
+                Console.WriteLine("index of the number {0}, minimum number {1}", Min(numbers), numbers(Min(numbers)))
+                Console.WriteLine("----------------------------------------------------------")
+            Case 3
+                Console.ForegroundColor = ConsoleColor.Green
+                Console.WriteLine("----------------------------------------------------------")
+                Console.WriteLine("index of the number {0}, maximum number {1}", Max(numbers), numbers(Max(numbers)))
+                Console.WriteLine("----------------------------------------------------------")
+            Case 4
+                Console.ForegroundColor = ConsoleColor.Green
+                Console.WriteLine("---------")
+                printRevers(numbers)
+                Console.WriteLine("---------")
+            Case 5
+                Console.ForegroundColor = ConsoleColor.Green
+                Console.WriteLine("---------")
+                print(numbers)
+                Console.WriteLine("---------")
+            Case 6
+                Console.ForegroundColor = ConsoleColor.Green
+                Console.WriteLine("---------")
+                add(numbers, "adding", 0)
+                print(numbers)
+                Console.WriteLine("---------")
+            Case 7
+                Console.ForegroundColor = ConsoleColor.Green
+                Console.WriteLine("---------")
+                delete(numbers, 3)
+                print(numbers)
+                Console.WriteLine("---------")
+            Case 8
+
+                Sort(numbers, SortDirection.Vozrastanie)
+                Console.ForegroundColor = ConsoleColor.Green
+                Console.WriteLine("---------")
+                print(numbers)
+                Console.WriteLine("---------")
+            Case 9
+                Sort(numbers, SortDirection.Ybivanie)
+                Console.ForegroundColor = ConsoleColor.Green
+                Console.WriteLine("---------")
+                print(numbers)
+                Console.WriteLine("---------")
+
+
+        End Select
+    End Sub
+
+    Sub printRevers(list As Integer())
         For i = list.Length - 1 To 0 Step -1
             Console.WriteLine(list(i))
         Next
@@ -38,24 +264,65 @@ Module ÏîèñêÝëåìåíòàâÌàññèâåфвф
             Console.WriteLine(list(i))
         Next
     End Sub
-
     Sub print(list As String())
-
         For i = 0 To list.Length - 1
             Console.WriteLine(list(i))
         Next
     End Sub
-
-    Function find(list As Integer(), num As Integer) As Integer
-
+    Sub printReverse(list As String())
+        For i = list.Length - 1 To 0 Step -1
+            Console.WriteLine(list(i))
+        Next
+    End Sub
+    Function find(list As Integer(), n As Integer)
         For i = 0 To list.Length - 1
-            If list(i) = num Then
+            If list(i) = n Then
                 find = i
                 Exit Function
             End If
         Next
-
         find = -1
+    End Function
+
+    Function Max(list As Integer(), Optional start As Integer = 0) As Integer
+        Max = start
+        For i = start + 1 To list.Length - 1
+            If list(i) > list(Max) Then
+                Max = i
+            End If
+        Next
+    End Function
+    Function Min(list As Integer(), Optional start As Integer = 0) As Integer
+        Min = start
+        For i = start + 1 To list.Length - 1
+            If list(i) < list(Min) Then
+                Min = i
+            End If
+        Next
 
     End Function
-End Module
+
+    Sub add(ByRef list As Integer(), val As String, index As Integer)
+        If index < 0 Or index > list.Length Then Exit Sub
+        ReDim Preserve list(list.Length)
+        For i = list.Length - 1 To index + 1 Step -1
+            list(i) = list(i - 1)
+        Next
+
+        list(index) = val
+
+    End Sub
+    Sub delete(ByRef list As Integer(), index As Integer
+            End Sub
+    Sub BubbleSort(list As String(), Optional dir As SortType = SortType.Ascending)
+    Dim b As String
+    For j = 0 To list.Length - 2
+        For i = 0 To list.Length - 2 - j
+            If list(i) > list(i + 1) Then
+                b = list(i)
+                list(i) = list(i + 1)
+                list(i + 1) = b
+            End If
+        Next
+    Next
+End Sub
---
+++ Module34.vb
@@ -0,0 +1,149 @@
+﻿Module Module1
+    Dim brush As Char = "0"c
+    Sub Hline(leftX As Byte, topY As Byte, length As Byte)
+        Console.SetCursorPosition(leftX, topY)
+        For i = 0 To length - 1
+            Console.Write(brush)
+        Next
+    End Sub
+    Sub Vline(leftX As Byte, topY As Byte, length As Byte)
+        Console.SetCursorPosition(leftX, topY)
+        Console.Write(brush)
+        For i = 0 To length - 1
+            Console.SetCursorPosition(leftX, topY + i)
+            Console.Write(brush)
+        Next
+    End Sub
+    Sub FillRectMod(leftX As Byte, topY As Byte, width As Byte, height As Byte, x As Byte, y As Byte)
+        Console.SetCursorPosition(leftX, topY)
+        For i = 0 To height - 1
+            If i Mod (x + y) < x Then
+                Console.BackgroundColor = ConsoleColor.DarkBlue
+            Else
+                Console.BackgroundColor = ConsoleColor.White
+            End If
+            Hline(leftX, topY + i, width)
+        Next
+        Console.WriteLine()
+    End Sub
+    Sub FillRect(leftX As Byte, topY As Byte, width As Byte, height As Byte)
+        Console.SetCursorPosition(leftX, topY)
+        For i = 0 To height - 1
+            Hline(leftX, topY + i, width)
+        Next
+        Console.WriteLine()
+    End Sub
+    Sub Rect(leftX As Byte, topY As Byte, width As Byte, height As Byte)
+        Hline(leftX, topY, width)
+        Vline(leftX, topY, height)
+        Hline(leftX, topY + height, width)
+        Vline(leftX + width, topY, height + 1)
+        Console.WriteLine()
+    End Sub
+
+    Sub Main()
+        Console.SetWindowSize(100, 50)
+        Dim N As Integer, key As ConsoleKeyInfo, top, left, height, width, gap, quantity As Byte
+        Do
+            Console.Write("Что нарисовать: отметка - 1, отметка с цветом - 2, квадраты цветные - 3: ")
+            N = CInt(Console.ReadLine())
+
+
+            If N = 1 Then
+                Console.BackgroundColor = ConsoleColor.Black 'восстанавливаем цвет
+                Console.ForegroundColor = ConsoleColor.White
+                brush = "H"c
+                Console.Clear()
+                Console.WriteLine("Введите координаты отметки")
+                Console.Write("Координаты по вертикали: ")
+                top = CByte(Console.ReadLine)
+                Console.Write("Координаты по горизонтали: ")
+                left = CByte(Console.ReadLine)
+                Console.WriteLine("Введите размеры")
+                Console.Write("Длина: ")
+                height = CByte(Console.ReadLine)
+                Console.Write("Ширина: ")
+                width = CByte(Console.ReadLine)
+                Console.Clear()
+                height = height / 2
+
+
+                While width > 8 And height > 4
+                    Rect(left, top, width, height)
+                    left += 4
+                    top += 2
+                    width -= 8
+                    height -= 4
+                End While
+            End If
+
+
+            If N = 2 Then
+                Console.BackgroundColor = ConsoleColor.Black 'восстанавливаем цвет
+                Console.ForegroundColor = ConsoleColor.White
+                brush = "H"c
+                Console.Clear()
+                Console.WriteLine("Введите координаты отметки с цветом")
+                Console.Write("Координаты по вертикали: ")
+                top = CByte(Console.ReadLine)
+                Console.Write("Координаты по горизонтали: ")
+                left = CByte(Console.ReadLine)
+                Console.WriteLine("Введите размеры")
+                Console.Write("Длина: ")
+                height = CByte(Console.ReadLine)
+                Console.Write("Ширина: ")
+                width = CByte(Console.ReadLine)
+                Console.Clear()
+                height = height / 2
+
+                Dim colors = New ConsoleColor() {ConsoleColor.DarkYellow, ConsoleColor.Yellow, ConsoleColor.Blue, ConsoleColor.Red, ConsoleColor.Green, ConsoleColor.DarkBlue, ConsoleColor.DarkMagenta}
+
+                Dim i As Integer
+
+                While width > 8 And height > 4
+                    If i > colors.Length - 1 Then
+                        i = 0
+                    End If
+                    Console.ForegroundColor = colors(i)
+                    Rect(left, top, width, height)
+                    left += 4
+                    top += 2
+                    width -= 8
+                    height -= 4
+                    i += 1
+
+                End While
+                Console.BackgroundColor = ConsoleColor.Black 'восстанавливаем цвет
+                Console.ForegroundColor = ConsoleColor.White
+
+            End If
+
+            If N = 3 Then
+                brush = " "c
+                Console.BackgroundColor = ConsoleColor.Black 'восстанавливаем цвет
+                Console.ForegroundColor = ConsoleColor.White
+                Console.Clear()
+                Console.WriteLine("Введите координаты квадрата")
+                Console.Write("Координаты по вертикали: ")
+                top = CByte(Console.ReadLine)
+                Console.Write("Координаты по горизонтали: ")
+                left = CByte(Console.ReadLine)
+                Console.WriteLine("Введите размеры")
+                Console.Write("Длина: ")
+                height = CByte(Console.ReadLine)
+                Console.Write("Ширина: ")
+                width = CByte(Console.ReadLine)
+                Console.Clear()
+                height = height / 2
+                FillRectMod(left, top, width, height, 2, 2)
+                FillRectMod(left + width + 2, top, width, height, 3, 1)
+                FillRectMod(left, top + height + 2, width, height, 1, 3)
+                FillRectMod(left + width + 2, top + height + 2, width, height, 3, 2)
+
+                Console.BackgroundColor = ConsoleColor.Black 'воcстанавливаем цвет
+                Console.ForegroundColor = ConsoleColor.White
+            End If
+        Loop
+    End Sub
+
+End Module
