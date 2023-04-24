f18b88807975cc997dd34a683d69413224ed9a40
vitaliilavrentev22 <122769637+vitaliilavrentev22@users.noreply.github.com>
2023-04-24 08:42:19 +0300

    Add files via upload

---
+++ Module31.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Security.Policy
+
+Module Program
+
+    Sub Main(args As String())
+        Console.WriteLine(MathX(-2))
+        Console.WriteLine(MathX(7, 5))
+    End Sub
+    Function MathX(x As Double) As Double
+        Dim y As Double
+        If x > 0 Then
+            y = 2
+        Else
+            z = Math.Pow(x, 2)
+        End If
+        MathX = Math.Pow(Math.E, Cos(x, y)) - 2.7 * y
+    End Function
+
+
+End Module
